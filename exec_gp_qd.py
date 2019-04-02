f452a25450fb8c78cb8c6da561ab82efaf92cfc1
Jean-Philippe Bruneton <jpbruneton@gmail.com>
2019-04-02 17:26:39 +0200

    Update exec_gp_qd.py

--- exec_gp_qd.py
+++ exec_gp_qd.py
@@ -456,7 +456,8 @@ def main():
                 # this might directly provide the exact solution : if not, stop is None, and thus, run evolution
                 if stop is None:
                     gp.QD_pool = QD_pool
-                    iteration_a = 100
+                    iteration_a = 150
+          
                     stop, qdpool, alleqs_a, iter_a = exec(which_target, train_target, test_target, voc_with_a, iteration_a, tolerance, gp, prefix)
 
                     if stop is None:
