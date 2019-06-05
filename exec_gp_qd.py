1b16b7e466e6d8659b7c32063dab0e1003761359
Jean-Philippe Bruneton <jpbruneton@gmail.com>
2019-06-05 08:43:27 +0200

    Update exec_gp_qd.py

--- exec_gp_qd.py
+++ exec_gp_qd.py
@@ -609,7 +609,7 @@ def init_everything_else(which_target):
 # -----------------------------------------------#
 def main():
     id = str(int(10000000 * time.time()))
-    for u in range(13,24):
+    for u in range(16,24):
 
         # init target, dictionnaries, and meta parameters
         which_target = u
